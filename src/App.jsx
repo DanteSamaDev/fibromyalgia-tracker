7c46eebfb465aac2f57c5dbc036b69da122e5e79
DanteSamaDev <133621528+DanteSamaDev@users.noreply.github.com>
2026-02-19 15:48:54 -0300

    Create App.jsx with React App component code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Fibromyalgia Tracker</h1>
+            {/* Your components and code here */}
+        </div>
+    );
+};
+
+export default App;
